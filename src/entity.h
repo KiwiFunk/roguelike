5d616815d374af20e6e2d0868e7caaf7b9c52e21
Alex Price <alex_p97@live.com>
2026-06-14 10:12:24 +0100

    add entity manager to header

--- src/entity.h
+++ src/entity.h
@@ -4,9 +4,28 @@ typedef enum {
     ENTITY_TYPE_ITEM
 } entity_type;
 
+// Macros
+#define MAX_ENTITIES 100                // Cap for maximum entities in memory at once
+#define INITIAL_CAPACITY 10             // Initial size of entity array
+
+// Structures
 struct entity {
     int id;
     entity_type type;
     int x, y;
     int hp;
 };
+
+struct entity_manager {
+    entity entities[INITIAL_CAPACITY];
+    int count;                          // Track current number of entities
+    int capacity;                       // Track current capacity of array
+};
+
+// Entity manager functions
+struct entity_manager* init_entity_manager();
+void cleanup_entity_manager(struct entity_manager* manager);
+
+// Entity functions
+struct entity* create_entity(entity_type type, int x, int y);
+void destroy_entity(struct entity* e);
