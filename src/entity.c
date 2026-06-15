cc8f8f0a0a996d119355a2785c6f2f1062aa5c45
Alex Price <alex_p97@live.com>
2026-06-15 17:41:25 +0100

    create function to add entity to manager

--- src/entity.c
+++ src/entity.c
@@ -27,14 +27,46 @@ struct entity_manager* init_entity_manager() {
 }
 
 void add_entity(struct entity_manager *em, struct entity *e) {
-    // Check entity manager exists (not Null) and has space
-    // Handle case where entity manager is full (count >= capacity)
-    // If full, realloc
-    // Add memory address of e to em array and increment count
-     if (em == NULL) {
-        fprintf(stderr, "Entity manager is NULL\n");
+    // Null checks
+    if (em == NULL || e == NULL) {
+        fprintf(stderr, "Failed to add entity: Manager or Entity is NULL\n");
+        if (e) destroy_entity(e); // Clean up if entity valid but !manager
         return;
     }
+
+    // Check em has capacity for new entity (count < capacity)
+    if (em->count >= em->capacity) {
+
+        if (em->capacity >= MAX_ENTITIES) {
+            fprintf(stderr, "Cannot add entity: Reached maximum capacity of %d\n", MAX_ENTITIES);
+            destroy_entity(e);  // Clean up entity to avoid memory leak since failed to add to em
+            return;
+        }
+
+        int new_capacity = em->capacity * 2;
+        if (new_capacity > MAX_ENTITIES) {
+            new_capacity = MAX_ENTITIES;
+        }
+
+        // Realloc to a tempory pointer to avoid losing reference to original memory if realloc fails
+        struct entity *temp = realloc(em->entities, new_capacity * sizeof(struct entity));
+
+        // Gracefully handle realloc failure and clean up new enitiy with warning
+        if (temp == NULL) {
+            fprintf(stderr, "Failed to reallocate memory for entity array\n");
+            destroy_entity(e);
+            return;
+        }
+
+        // Update em with new address (realloc handles copying data and cleaning up old memory)
+        em->entities = temp;
+        em->capacity = new_capacity;
+        printf("Em capacity increased to %d\n", em->capacity);
+    }
+
+    // Add new entity to the array and increment count
+    em->entities[em->count] = *e;
+    em->count++;
 }
 
 void cleanup_entity_manager(struct entity_manager* em) {
@@ -53,5 +85,9 @@ struct entity* create_entity(entity_type type, int x, int y) {
 }
 
 void destroy_entity(struct entity *e) {
-
+    // Destroy entity e and free memory
+     if (e) {
+        free(e);
+    }
+     e = NULL; // Avoid dangling pointer
 }
