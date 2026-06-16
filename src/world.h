649d389d62965f553bac10c332fbd4c6e4054183
Alex Price <alex_p97@live.com>
2026-06-16 22:41:14 +0100

    create world.c, world.h

---
+++ src/world.h
@@ -0,0 +1,17 @@
+typedef enum {
+    TILE_FLOOR,
+    TILE_WALL,
+    TILE_CORRIDOR
+} tile_type;
+
+struct world {
+    int width, height;
+    tile_type **tiles;      // 2D Array [y][x]
+};
+
+struct world* world_create(int w, int h);
+void world_destroy(struct world* w);
+
+tile_type world_get_tile(struct world* w, int x, int y);
+void world_set_tile(struct world* w, int x, int y, tile_type type);
+bool is_walkable(struct world* w, int x, int y);
