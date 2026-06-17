31a0f918d52bfec9401787432ba6410e70584f0f
Alex Price <alex_p97@live.com>
2026-06-17 12:33:38 +0100

    create getter setter for world tiles

--- src/world.c
+++ src/world.c
@@ -4,14 +4,12 @@
 
 struct world* world_create(int w, int h) {
 
-    // Allocate memory for the world struct
     struct world* new_world = malloc(sizeof(struct world));
     if (!new_world) {
         fprintf(stderr, "Failed to allocate memory for world\n");
         return NULL;
     }
 
-    // Allocate memory for the tiles array
     tile_type *tiles = malloc(w * h * sizeof(tile_type));
     if (!tiles) {
         fprintf(stderr, "Failed to allocate memory for tiles\n");
@@ -19,10 +17,37 @@ struct world* world_create(int w, int h) {
         return NULL;
     }
 
-    // Set properties
     new_world->width = w;
     new_world->height = h;
     new_world->tiles = tiles;
 
-    return new_world;               // Return pointer to the newly created world
+    return new_world;                       // Return pointer to the newly created world
+}
+
+void world_destroy(struct world* w) {
+    if (!w) return;
+
+    free(w->tiles);                         // Free the tiles array
+    free(w);                                // Free the world struct
+}
+
+tile_type world_get_tile(struct world* w, int x, int y) {
+
+    if (!w || x < 0 || y < 0 || x >= w->width || y >= w->height) {
+        fprintf(stderr, "Invalid coordinates for world_get_tile\n");
+        return NULL;                        // Return NULL so calling function can handle
+    }
+
+    return w->tiles[(y * w->width) + x];    // i = (y * width) + x
+}
+
+void world_set_tile(struct world* w, int x, int y, tile_type type) {
+
+    if (!w || x < 0 || y < 0 || x >=w->width || y >= w->height) {
+        fprintf(stderr, "Invalid coordinates for world_set_tile\n");
+        return;
+    }
+
+    w->tiles[(y * w->width) + x] = type;
+    return;
 }
